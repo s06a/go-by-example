7876de250573ed8543e6fdab2b530b4269602fcb
s06a <91134347+s06a@users.noreply.github.com>
2024-06-14 21:31:22 +0330

    [Update] better documentation

--- examples and notes/13_closure.go
+++ examples and notes/13_closure.go
@@ -1,12 +1,13 @@
 package main
 
-// closures are functions that can access or manipulate
-// values from the outer function. also, here we use
-// anonymous function, which is useful when you want to
-// define a function without a name.
+// closures are functions that can access or manipulate values
+// from the outer function. also, here we use anonymous function,
+// which is useful when you want to define a function without a name.
 
 import "fmt"
 
+// intSeq returns a closure that generates sequential integers.
+// The closure keeps track of the last integer and increments it.
 func intSeq() func() int {
 	i := 0
 	return func() int {
@@ -15,10 +16,8 @@ func intSeq() func() int {
 	}
 }
 
-// factorial function returns a function which gets
-// integer and returns integer. first we define result
-// as a value in the outer function, then we return the
-// inner function which uses result as a value.
+// factorial returns a closure that calculates the factorial of a given integer.
+// The inner function maintains the result across multiple calls.
 func factorial() func(int) int {
 	var result int
 	return func(num int) int {
@@ -33,8 +32,8 @@ func factorial() func(int) int {
 	}
 }
 
-// this function gets factor and returns a function
-// which gets integer (num) and returns integer.
+// multiplyBy returns a closure that multiplies a given factor with a number.
+// The returned function accepts an integer and returns the product.
 func multiplyBy(factor int) func(int) int {
 	return func(num int) int {
 		return factor * num
@@ -42,7 +41,7 @@ func multiplyBy(factor int) func(int) int {
 }
 
 func main() {
-	// next integer
+	// Create a closure for generating sequential integers
 	nextInt := intSeq()
 
 	fmt.Println(nextInt())
@@ -52,13 +51,13 @@ func main() {
 	newNextInt := intSeq()
 	fmt.Println(newNextInt())
 
-	// factorial function
+	// Create a closure for calculating factorial
 	returnFactorial := factorial()
 
 	fmt.Println(returnFactorial(5))
 	fmt.Println(returnFactorial(10))
 
-	// multiplyBy function
+	// Create a closure for multiplying by two
 	multiplyByTwo := multiplyBy()
 	fmt.Println(multiplyByTwo(5))
 }
