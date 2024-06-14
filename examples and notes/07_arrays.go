f82af69a4496d90a76a9060c95b89e91eaed9e3b
s06a <91134347+s06a@users.noreply.github.com>
2024-06-14 14:52:36 +0330

    [Add] arrays

---
+++ examples and notes/07_arrays.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a [5]int
+	fmt.Println(a) // default value is array of zeros
+
+	a[4] = 10
+	fmt.Println(a)
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(b)
+
+	// auto count
+	c := [...]int{1, 2, 3}
+	fmt.Println(c)
+
+	d := [...]int{2: 1, 1: 2}
+	fmt.Println(d)
+
+	var twoD [4][4]int
+	for i := range 4 {
+		for j := range 4 {
+			twoD[i][j] = i + j
+		}
+	}
+	fmt.Println(twoD)
+	fmt.Println(twoD[2][:]) // third row
+}
